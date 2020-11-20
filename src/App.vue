188dbcc3fe1629ac700885395944e424041eeb83
ehpark <ehpark@mcnc.co.kr>
2020-11-20 18:01:22 +0900

    vutifiy

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <v-app>
+
+    <v-navigation-drawer app></v-navigation-drawer>
     <v-app-bar app></v-app-bar>
+
     <v-main>
-      <v-container></v-container>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-main>
+
+    <v-footer app></v-footer>
+
   </v-app>
 </template>
 
 <script>
+//import NavigationDrawer from '@/views/layout/NavigationDrawer.vue';
+//import AppBar from '@/views/layout/AppBar.vue';
+//import Footer from '@/views/layout/Footer.vue';
+
 export default {
   name: 'App',
 
-  components: {},
+  components: {
+    
+  },
 
   data: () => ({
 
