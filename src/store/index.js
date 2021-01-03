fd3206a37f76a91d7c0edcd415ea474fe6fe7e02
박은하 <momo@bag-eunhaui-MacBookAir.local>
2021-01-03 18:43:44 +0900

    update

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,25 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 
+import auth from './auth.js'
 import constants from './constants.js'
 
 Vue.use(Vuex)
 
-/* eslint-disable no-new */
-const store = new Vuex.Store({
-  plugins: [createPersistedState()],
+export default new Vuex.Store({
   modules: {
+    auth,
+    
   },
+  plugins: [
+    createPersistedState({paths: ["auth"]})
+  ],
   state : {
     constants,
-    drawer:false
+    drawer:false,
+    title:"app.."
   },
   getters : {
     CONST : state =>{ return state.constants }
   }
 })
-
-export default store
